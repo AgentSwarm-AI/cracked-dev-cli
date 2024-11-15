4ed27c3ebc4a5a83b161c6b08eb0b93b66bfb70b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-11-15 14:41:59 -0800

    add reflect-metadata and tsyringe for dependency injection; refactor sum function to use Calculator class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const sum = (a: number, b: number): number => a + b;
+import 'reflect-metadata';
+import { autoInjectable } from 'tsyringe';
+
+@autoInjectable()
+class Calculator {
+  sum(a: number, b: number): number {
+    return a + b;
+  }
+}
+
+const calculator = new Calculator();
+
+export const sum = (a: number, b: number): number => {
+  return calculator.sum(a, b);
+};
